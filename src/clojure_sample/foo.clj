a395c4aed4646217bdc3081b46a4968301b1fe61
Noah Bogart <noah.bogart@hey.com>
2024-03-04 10:08:15 -0500

    Add cond issue

--- src/clojure_sample/foo.clj
+++ src/clojure_sample/foo.clj
@@ -4,3 +4,23 @@
   ^{:doc "All registered rules, grouped by :init-type and full-name"}
   global-rules
   (atom {:rules {} :genres #{}}))
+
+(defn deps-from-libspec
+  "A modification from clojure.tools.namespace.parse/deps-from-libspec."
+  [prefix form]
+  (cond (prefix-spec? form)
+    (mapcat (fn [f] (deps-from-libspec
+                      (symbol (str (when prefix (str prefix "."))
+                                (first form)))
+                      f))
+      (next form))
+
+    (option-spec? form)
+    (let [opts (apply hash-map (next form))]
+      [{:ns (symbol (str (when prefix (str prefix ".")) (first form)))
+        :alias (or (:as opts) (:as-alias opts))}])
+
+    (js-dep-spec? form)
+    (let [opts (apply hash-map (next form))]
+      [{:ns (str (when prefix (str prefix ".")) (first form))
+        :alias (or (:as opts) (:as-alias opts))}])))
